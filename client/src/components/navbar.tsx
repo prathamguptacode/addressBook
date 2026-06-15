9135a5985021b222c23154403e6d4e811e13473b
Pratham Gupta <prathamgupta.wk@gmail.com>
2026-06-15 21:49:42 +0530

    done

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -1,11 +1,22 @@
 import mystyle from './navbar.module.css'
-import { BookUser, UserRoundPlus } from 'lucide-react'
+import { BookUser, UserRoundPlus, X } from 'lucide-react'
 import { Input } from './retroui/Input'
 import { Button } from './retroui/Button'
 import { useNavigate } from 'react-router'
 import { useEffect, useState } from 'react'
 import { useQuery } from '@tanstack/react-query'
 import { api } from '@/api/api'
+import clsx from 'clsx'
+
+
+type userT = {
+  username: string,
+  Block: string,
+  room: string
+}
+type resT = {
+  users: userT[] | null
+}
 
 function Navbar() {
 
@@ -13,7 +24,7 @@ function Navbar() {
   const [query, setQuery] = useState("")
   const { data, refetch } = useQuery({
     queryKey: ["search", query],
-    queryFn: () => api.get(`/search?q=${query}`),
+    queryFn: () => api.get<resT>(`/search?q=${query}`),
     enabled: false
   })
   function reqSearch() {
@@ -21,8 +32,6 @@ function Navbar() {
       refetch()
     }
   }
-  console.log(data)
-
 
   useEffect(() => {
     const idT = setTimeout(() => {
@@ -41,7 +50,49 @@ function Navbar() {
         <div className={mystyle.logo}>Address Book</div>
       </div>
       <div className={mystyle.searchBox}>
-        <Input placeholder='search...' type='text' onChange={e => setQuery(e.currentTarget.value)} />
+        <Input placeholder='search...' type='text' onChange={e => setQuery(e.currentTarget.value)}
+          onFocus={() => {
+            const elm = document.getElementById("pop")
+            if (elm) {
+              elm.showPopover()
+            }
+          }}
+        // onBlur={() => {
+        //   const elm = document.getElementById("pop")
+        //   if (elm) {
+        //     elm.hidePopover()
+        //   }
+        // }}
+        />
+        {
+          query ?
+            <button className={mystyle.closeBtn} onClick={() => {
+              const elm = document.getElementById("pop")
+              if (elm) {
+                elm.hidePopover()
+              }
+            }}><X /></button>
+            : null
+        }
+        <div id="pop" popover='manual' className={clsx(data ? mystyle.pop : null)}>
+          {
+            data ? data.data.users == null ? <div className='px-[12px]' onClick={() => console.log("cat")}>No user found</div>
+              :
+              data.data.users.map((e) => {
+                return <div className={mystyle.opt} onClick={() => navigate(`/profile?name=${e.username}&block=${e.Block}&room=${e.room}`)}>
+                  <div>{e.username}</div>
+                  <div className='flex justify-between'>
+                    <div>
+                      {e.room}
+                    </div>
+                    <div>
+                      {e.Block}
+                    </div>
+                  </div>
+                </div>
+              }) : null
+          }
+        </div>
       </div>
       <div>
         <Button onClick={() => navigate("/form")}>
--- server/go.mod
+++ server/go.mod
@@ -3,15 +3,19 @@ module github.com/prathamguptacode/addressBook
 go 1.26.3
 
 require (
+	github.com/gofiber/fiber/v3 v3.3.0
+	github.com/joho/godotenv v1.5.1
+	go.mongodb.org/mongo-driver/v2 v2.6.0
+	golang.org/x/oauth2 v0.36.0
+)
+
+require (
 	cloud.google.com/go/compute/metadata v0.3.0 // indirect
 	github.com/andybalholm/brotli v1.2.1 // indirect
-	github.com/gofiber/fiber/v3 v3.3.0 // indirect
 	github.com/gofiber/schema v1.7.1 // indirect
 	github.com/gofiber/utils/v2 v2.0.6 // indirect
 	github.com/google/uuid v1.6.0 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/klauspost/compress v1.18.6 // indirect
-	github.com/markbates/goth v1.82.0 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.22 // indirect
 	github.com/philhofer/fwd v1.2.0 // indirect
@@ -22,10 +26,8 @@ require (
 	github.com/xdg-go/scram v1.2.0 // indirect
 	github.com/xdg-go/stringprep v1.0.4 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20240726163527-a2c0da244d78 // indirect
-	go.mongodb.org/mongo-driver/v2 v2.6.0 // indirect
 	golang.org/x/crypto v0.51.0 // indirect
 	golang.org/x/net v0.54.0 // indirect
-	golang.org/x/oauth2 v0.36.0 // indirect
 	golang.org/x/sync v0.20.0 // indirect
 	golang.org/x/sys v0.44.0 // indirect
 	golang.org/x/text v0.37.0 // indirect
